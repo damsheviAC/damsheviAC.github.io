03cc347da275d6033d318deb52c6d3c8b28c1183
damsheviAC <aiawww@mail.ru>
2018-08-29 10:32:02 +0300

    update of expert

--- expert/js/main.js
+++ expert/js/main.js
@@ -20,13 +20,18 @@ $(document).ready(function () {
     });
 
 
+
+
     //input radio
     $('.input__container').find('input').on('change', function () {
       $(this).next().toggleClass('chacked'); 
       $('.input__container input').not(this).next().removeClass('chacked');     
     });
 
-    //slider
+
+
+
+    //slider partner
     $('.partners__container').slick({
       dots: true,
       arrows: false,
@@ -62,5 +67,75 @@ $(document).ready(function () {
       },
       ]
     });
+
+
+
+
+    //slider feedback official
+    $('.container_feedback_official').slick({
+      dots: true,
+      arrows: false,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+      {
+        breakpoint: 1201,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          arrows: false,
+          dots: true,
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: true,
+        }
+      },
+      ]
+    });   
+
+
+
+
+
+
+
+    //slider feedback
+    $('.container_feedback').slick({
+      dots: true,
+      arrows: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    }); 
+
+
+
+
+    //click exit
+    $('.exit').on('click', function(){
+      $(this).parent().css('opacity', '0');
+      $(this).parent().css('z-index', '-1');
+    });
+
+    $('#main_button button, #services_button button').on('click', function(){
+      $('#zapros').css('opacity', '1');
+      $('#zapros').css('z-index', '2');
+    });
+
+    $('#feedback_button button').on('click', function(){
+      $('#otzyv').css('opacity', '1');
+      $('#otzyv').css('z-index', '2');
+    });
+
+    $('#documentation_button button').on('click', function(){
+      $('#el-zapros').css('opacity', '1');
+      $('#el-zapros').css('z-index', '2');
+    });
+
 
 });
